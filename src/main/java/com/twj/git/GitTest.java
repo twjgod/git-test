85e11904220fa3e27f8a770f4f1e06277d1320a2
tangwenjie <doujiao.twj@raycloud.com>
2022-07-03 11:49:06 +0800

    gitee push

--- src/main/java/com/twj/git/GitTest.java
+++ src/main/java/com/twj/git/GitTest.java
@@ -9,6 +9,6 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("11111");
         System.out.println("11111222");
-        
+        System.out.println("gitee");
     }
 }
