f52f0fb1fa3e2fd19c688c6fb37fd5f2775f6189
tangwenjie <doujiao.twj@raycloud.com>
2022-07-09 13:31:57 +0800

    gitmaster

--- src/main/java/com/twj/git/GitTest.java
+++ src/main/java/com/twj/git/GitTest.java
@@ -11,5 +11,7 @@ public class GitTest {
         System.out.println("11111222");
         System.out.println("gitee");
         System.out.println("gitee pull");
+        System.out.println("gitmaster");
     }
+
 }
