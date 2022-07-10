c3b04d5ee6e7b0e2ca60bef9b928eb72864af88c
tangwenjie <doujiao.twj@raycloud.com>
2022-07-10 11:04:48 +0800

    idea  fenz

--- src/main/java/com/twj/git/GitTest.java
+++ src/main/java/com/twj/git/GitTest.java
@@ -15,6 +15,7 @@ public class GitTest {
         System.out.println("test111");
         System.out.println("tess222");
         System.out.println("git fetch");
+        System.out.println("idea branch");
     }
 
 }
